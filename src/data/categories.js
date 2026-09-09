5188e94cba21c04d2eefc11491fea507e8bc5ef4
DJGiulo <giulianolezcano57@gmail.com>
2026-09-09 16:35:08 -0300

    correccion ortografica de categoria nueva

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,1 +1,1 @@
-export const categories = ['Biyutería holística', 'Sahumerios', 'Budas', 'Atrapasueños', 'Cannabis', 'Esencias/difusores', 'Velas aromáticas', 'Conos cascada', 'Tapices' ]
+export const categories = ['Bisutería holística', 'Sahumerios', 'Budas', 'Atrapasueños', 'Cannabis', 'Esencias/difusores', 'Velas aromáticas', 'Conos cascada', 'Tapices' ]
